0f68659f39848a1e132fedbfc2554b01e7f75341
Matt Hoyt <mrhoyt4@gmail.com>
2019-05-21 18:41:02 -0500

    Add a binary heap.

---
+++ Mrh.Concurrent/SimpleRetry.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Mrh.Concurrent
+{
+    public class SimpleRetry : IRetryService
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Retry(TimeSpan retry, Action act)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mrh.DataStructure.Test/BinaryHeapTest.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Mrh.DataStructures;
+using NUnit.Framework;
+
+namespace Mrh.DataStructure.Test
+{
+    [TestFixture]
+    public class BinaryHeapTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            var rand = new Random(0);
+            var num = 10000;
+            var heap = new BinaryHeap<int>(16, new IntComaparer());
+            var values = new List<int>(100);
+            for (var i = 0; i < 100; i++)
+            {
+                var newNum = rand.Next(100);
+                heap.Put(newNum);
+                values.Add(newNum);
+            }
+
+            var prev = -1;
+            Assert.IsTrue(heap.VerifyHeap(0));
+            values.Sort(new IntComaparer());
+            for (var i = 0; i < 100; i++)
+            {
+                int c = 0;
+                Assert.IsTrue(heap.Take(out c));
+                Assert.LessOrEqual(prev, c);
+                Assert.AreEqual(values[i], c);
+                prev = c;
+            }
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            var num = 1000;
+            for (var i = 0; i < num; i++)
+            {
+                var rand = new Random(0);
+                var heap = new BinaryHeap<int>(16, new IntComaparer());
+                var values = new List<int>(100);
+                for (var j = 0; j < 100; j++)
+                {
+                    var newNum = rand.Next(100);
+                    heap.Put(newNum);
+                    values.Add(newNum);
+                }
+
+                var prev = -1;
+                Assert.IsTrue(heap.VerifyHeap(0));
+                values.Sort(new IntComaparer());
+                for (var j = 0; j < 100; j++)
+                {
+                    int c = 0;
+                    Assert.IsTrue(heap.Take(out c));
+                    Assert.LessOrEqual(prev, c);
+                    Assert.AreEqual(values[j], c);
+                    prev = c;
+                }
+            }
+        }
+
+        private class IntComaparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
