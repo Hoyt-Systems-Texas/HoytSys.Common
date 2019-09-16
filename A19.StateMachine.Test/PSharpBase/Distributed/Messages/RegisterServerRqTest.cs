6c377c9010c5827adae2aef19255a64c2aef51cc
Matt Hoyt <mrhoyt4@gmail.com>
2019-09-16 14:39:26 -0500

    Have an example of the encoder/decoder working.

--- A19.StateMachine.Test/PSharpBase/Distributed/Messages/RegisterServerRqTest.cs
+++ A19.StateMachine.Test/PSharpBase/Distributed/Messages/RegisterServerRqTest.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel;
 using System.Text;
 using A19.StateMachine.PSharpBase.Distributed.Messages;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 using NUnit.Framework;
 using Org.SbeTool.Sbe.Dll;
 
@@ -15,11 +17,29 @@ namespace Mrh.StateMachine.Test.PSharpBase.Distributed.Messages
         {
             var buffer = new byte[1000];
             var directBuffer = new DirectBuffer(buffer);
+            var header = new MessageHeader();
+            header.Wrap(directBuffer, 0, RegisterServerRq.SchemaVersion);
+            header.BlockLength = RegisterServerRq.BlockLength;
+            header.SchemaId = RegisterServerRq.SchemaId;
+            header.TemplateId = RegisterServerRq.TemplateId;
+            header.Version = RegisterServerRq.SchemaVersion;
+
+            var currentOffset = MessageHeader.Size;
             var registerServer = new RegisterServerRq();
-            registerServer.WrapForEncode(directBuffer, 0);
+            registerServer.WrapForEncode(directBuffer, currentOffset);
             var hostName = "Hello";
-            registerServer.Server = 1;
+            registerServer.Server = 2;
             registerServer.SetUri(Encoding.UTF8.GetBytes(hostName), 0, hostName.Length);
+            
+            header.Wrap(directBuffer, 0, RegisterServerRq.SchemaVersion);
+            var decodeServer = new RegisterServerRq();
+            currentOffset = MessageHeader.Size;
+            decodeServer.WrapForDecode(new DirectBuffer(buffer), currentOffset, header.BlockLength, header.Version);
+            Assert.AreEqual(2, decodeServer.Server);
+            var hostByte = new byte[10000];
+            var length = decodeServer.GetUri(hostByte, 0, hostByte.Length);
+            Assert.AreEqual(5, length);
+            Assert.AreEqual("Hello", Encoding.UTF8.GetString(hostByte, 0, length));
         }
     }
 }
