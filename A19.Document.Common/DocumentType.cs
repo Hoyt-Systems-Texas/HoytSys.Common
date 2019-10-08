18e80a64351401f4fb8dbdf07c567479c67093fa
Matt Hoyt <mrhoyt4@gmail.com>
2019-10-07 22:42:12 -0500

    Add a document type.

---
+++ A19.Document.Common/DocumentType.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using A19.Core;
+
+namespace A19.Document.Common
+{
+    public class DocumentType : IValidatable
+    {
+        public int DocumentTypeId { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public List<ValidationError> Validate(List<ValidationError> errors)
+        {
+            errors
+                .Required(nameof(Name), Name)
+                .MaxLength(100, nameof(Name), Name)
+                .Required(nameof(Description), Description)
+                .MaxLength(200, nameof(Description), Description)
+                ;
+            return errors;
+        }
+    }
+}
